8e082ad1eb68728c24523446bc1931968424e20d
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-05-02 00:21:11 +0300

    Create Feature model

--- features/api/models.py
+++ features/api/models.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Feature(models.Model):
+    PRODUCT_AREA_CHOICES = (
+        ('P', 'Policies'),
+        ('B', 'Billing'),
+        ('C', 'Claims'),
+        ('R', 'Reports'),
+    )
+
+    title = models.CharField(max_length=255, blank=False)
+    description = models.TextField()
+    target_date = models.DateField(blank=True)
+    ticket_url = models.URLField()
+    product_area = models.CharField(
+        max_length=2,
+        choices=PRODUCT_AREA_CHOICES,
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
--- features/api/tests.py
+++ features/api/tests.py
@@ -3,4 +3,28 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from api.models import Feature
+from datetime import datetime
+
+class FeatureModelTestCase(TestCase):
+    """
+    This class defines test cases for the Feature model
+    """
+
+    def setUp(self):
+        self.feature_title = "Write the Features APP"
+        self.feature_description = "Feature request description"
+        self.feature_target_date = datetime(year=2018, month=6, day=1)
+        self.feature_ticket_url = "www.example.com"
+        self.feature_product_area = "Policies"
+        self.feature = Feature(title=self.feature_title,
+                            description=self.feature_description,
+                            target_date=self.feature_target_date,
+                            ticket_url=self.feature_ticket_url,
+                            product_area=self.feature_product_area)
+
+    def test_model_creates_a_feature(self):
+        before_count = Feature.objects.count()
+        self.feature.save()
+        after_count = Feature.objects.count()
+        self.assertNotEqual(before_count, after_count)
